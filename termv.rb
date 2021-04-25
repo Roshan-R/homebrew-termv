a42c0f2f5c8f39ae7e6dc4ab25c99649a6a00375
Roshan R Chandar <43182697+Roshan-R@users.noreply.github.com>
2021-04-25 21:04:22 +0530

    Add files via upload

---
+++ termv.rb
@@ -0,0 +1,16 @@
+class Termv < Formula
+  desc "A terminal iptv player written in bash"
+  homepage "https://github.com/Roshan-R"
+  url "https://github.com/Roshan-R/termv/archive/refs/tags/v1.0.zip"
+  sha256 "060edcf0bc84cbf30594da190b656e003d88b788a1a24aab22d7ab2c13982970"
+  license ""
+
+  depends_on "mpv"
+  depends_on "jq"
+  depends_on "fzf"
+  depends_on "make" => :build
+
+  def install
+    system "make", "install"
+  end
+end
